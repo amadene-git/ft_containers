50e23212274bc1739a439e48503d4eff8c28975a
amadene-git <adioumd@hotmail.fr>
2022-01-27 23:01:08 +0100

    jeu. 27 janv. 2022 13:52:06 CET

--- vector/objs/main.d
+++
@@ -1,2 +0,0 @@
-objs/main.o: main.cpp vector.hpp Vector_Iterator.hpp iterator.hpp \
-  utils.hpp
--- vector/objs/vector_test.d
+++
@@ -1,2 +0,0 @@
-objs/vector_test.o: vector_test.cpp vector.hpp Vector_Iterator.hpp \
-  iterator.hpp utils.hpp
